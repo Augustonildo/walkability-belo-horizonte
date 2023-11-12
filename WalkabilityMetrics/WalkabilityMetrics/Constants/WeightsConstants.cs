f860744051487abd5bbb5a711e6fcf0027a1f434
augustonildo <augustocarvalhopp@gmail.com>
2023-11-12 10:13:18 -0300

    Updating Constant Weights

--- WalkabilityMetrics/WalkabilityMetrics/Constants/WeightsConstants.cs
+++ WalkabilityMetrics/WalkabilityMetrics/Constants/WeightsConstants.cs
@@ -2,13 +2,13 @@
 {
     public static class WeightsConstants
     {
-        public static readonly double PesoDeclividade = 0.25;
-        public static readonly double PesoPracaOuParque = 0.1;
-        public static readonly double PesoIluminacao = 0.15;
-        public static readonly double PesoAtividadesEconomicas = 0.05;
-        public static readonly double PesoMeioFio = 0.1;
-        public static readonly double PesoPavimentacao = 0.1;
-        public static readonly double PesoClassificacaoViaria = 0.25;
+        public static readonly double PesoDeclividade = 0.1344;
+        public static readonly double PesoPracaOuParque = 0.1376;
+        public static readonly double PesoIluminacao = 0.1631;
+        public static readonly double PesoAtividadesEconomicas = 0.1061;
+        public static readonly double PesoMeioFio = 0.1650;
+        public static readonly double PesoPavimentacao = 0.1493;
+        public static readonly double PesoClassificacaoViaria = 0.1445;
         // A soma dos pesos acima deve sempre ser igual a 1.
 
         public static readonly int[] DeclividadeMarcos = { 0, 5, 10, 20, 40 };
