3a54c42a5c11e950aa0e338fb676f40684bb652a
augustonildo <augustocarvalhopp@gmail.com>
2023-11-24 17:40:18 -0300

    That's it

--- WalkabilityMetrics/WalkabilityMetrics/Constants/ConnectionConstants.cs
+++ WalkabilityMetrics/WalkabilityMetrics/Constants/ConnectionConstants.cs
@@ -2,6 +2,6 @@
 {
     public static class ConnectionConstants
     {
-        public static readonly string connectionString = "Host=localhost;Port=5433;Database=postgres;Username=augusto;Password=admin";
+        public static readonly string connectionString = "Host=localhost;Port=5433;Database=<database_name>;Username=<your_user_name>;Password=<your_password>";
     }
 }
